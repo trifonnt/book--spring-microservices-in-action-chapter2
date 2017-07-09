55d59b9846b980257148a0e33fb26a84bf1e0aa6
Trifon <trifont@gmail.com>
2017-07-09 18:56:39 +0300

    Added MVN wrapper.

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/controllers/LicenseServiceController.java
@@ -1,20 +1,21 @@
 package com.thoughtmechanix.licenses.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.thoughtmechanix.licenses.model.License;
 import com.thoughtmechanix.licenses.services.LicenseService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 @RequestMapping(value="v1/organizations/{organizationId}/licenses")
 public class LicenseServiceController {
-    @Autowired
+    @SuppressWarnings("unused")
+	@Autowired
     private LicenseService licenseService;
 
     @RequestMapping(value="/{licenseId}",method = RequestMethod.GET)
